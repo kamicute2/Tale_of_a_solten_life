b2824d20075606040d7a9c33013b6e538a5a7c2e
Yaroslav Beltsev <belcev.76@mail.ru>
2020-10-11 22:02:58 +0300

    first commit

---
+++ Tale_of_a_stolen_life/Assets/Scripts/Move_Script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_Script : MonoBehaviour
+{
+    public Vector2 speed = new Vector2(3, 1);
+    public Vector2 direction = new Vector2(-1, 0);
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        movement = new Vector2(speed.x * direction.x, speed.y * direction.y);
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = movement;
+        rb.gravityScale = 0;
+        rb.drag = 9;
+    }
+}
---
+++ Tale_of_a_stolen_life/Assets/Scripts/Player_Control.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Control : MonoBehaviour
+{
+    public Vector2 speed = new Vector2(3, 1);
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        float movex = Input.GetAxis("Horizontal");
+        float movey = Input.GetAxis("Vertical");
+        movement = new Vector2(speed.x * movex, speed.y * movey);
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = movement;
+        rb.gravityScale = 0;
+        rb.drag = 9;
+    }
+}
